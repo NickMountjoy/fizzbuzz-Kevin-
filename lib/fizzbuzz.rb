beb2f0a429139d8adf5385df7f7fb6ac4d3fcb9d
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-22 12:42:55 +0000

    Fizzbuzz Kevin + Nick's road trip

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,7 @@
 def fizzbuzz(number)
-	'fizz'
+	return "Fizzbuzz" if number % 15 == 0
+	return "Fizz" if number % 3 == 0
+	return "Buzz" if number % 5 == 0
+	return number 
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,29 @@
 require_relative '../lib/fizzbuzz'
 
-describe 'fizzbuzz' do 
-	it 'returns "fizz" when passed 3' do
-		expect(fizzbuzz(3)).to eq 'fizz'
+	describe 'fizzbuzz' do 
+		it 'returns "Fizz" when passed 3' do
+		expect(fizzbuzz(3)).to eq 'Fizz'
 	end
+
+		it 'returns "Buzz" when passed 5' do
+		expect(fizzbuzz(5)).to eq 'Buzz'
+	end
+    
+		it 'returns "Fizz" when passed a multiple of 3' do
+		expect(fizzbuzz(12)).to eq 'Fizz'
+	end
+
+		it 'returns "Buzz" when passed a multiple of 5' do
+		expect(fizzbuzz(25)).to eq 'Buzz'
+	end
+
+		it 'returns "Fizzbuzz" when passed a multiple of both 3 and 5' do
+		expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+	end
+
+		it 'returns number when not a multiple of 3, 5' do
+		expect(fizzbuzz(4)).to eq 4
+		expect(fizzbuzz(11)).to eq 11
+	end
+
 end
